16423df4c058ed748623b2a6efd59e6ae7f05fa6
hajara-221115 <n221115@rguktn.ac.in>
2026-02-11 16:37:51 +0530

    File Manager(functions and modes) uploaded

--- register.php
+++ register.php
@@ -3,8 +3,8 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $username = trim($_POST['username']);
-    $password = trim($_POST['password']);
+    $username = trim($_POST['Name']);
+    $password = trim($_POST['Email']);
 
     // Validation
     if (strlen($username) < 4) {
